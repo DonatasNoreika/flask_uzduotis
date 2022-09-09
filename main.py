26f21527ed97fdb6a71943cf20da9e9f4502ae02
Donatas Noreika <donoras@gmail.com>
2022-09-09 08:27:31 +0300

    pridėta keliamieji.html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from calendar import isleap
 
 app = Flask(__name__)
 
@@ -9,6 +10,11 @@ def home():
 @app.route("/address/<vardas>")
 def address(vardas):
     return render_template('address.html', vardas=vardas)
+
+@app.route("/keliamieji")
+def keliamieji():
+    masyvas = [metai for metai in range(1900, 2100) if isleap(metai)]
+    return render_template('keliamieji.html', metai=masyvas)
 
 if __name__ == "__main__":
     app.run(debug=True)
